1f9e1dc02bc869790e77c50bfb2b32674233020b
pdubsmcgee <151684842+pdubsmcgee@users.noreply.github.com>
2026-02-26 18:00:37 -0500

    Improve SEO metadata and automate sitemap generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://foremostmachine.com',
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    sitemap({
+      filter: (page) => !page.includes('/portal/')
+    })
+  ],
   output: 'static'
 });
